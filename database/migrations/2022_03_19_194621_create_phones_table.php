10a6be0494229f56139e677163cbc394dd2948b1
wayn3r <w9delacruz@gmail.com>
2022-03-19 18:07:26 -0400

    add migrations

---
+++ database/migrations/2022_03_19_194621_create_phones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('phoneNumber', 10);
+            $table->foreignId('contactId');
+            $table->foreign('contactId')->references('id')->on('contacts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
---
+++ database/migrations/2022_03_19_195204_create_addresses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('address', 255);
+            $table->foreignId('contactId');
+            $table->foreign('contactId')->references('id')->on('contacts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
